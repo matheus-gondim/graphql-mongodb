e942dec6ff33c12ba9247388aaea3352dbb4eb6a
matheus-gondim <matheus-gondim>
2021-03-09 23:58:33 -0300

    add ggraphQl package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
+// autoSchemaFile => define onde os schemas seram salvos, se true, eles seram salvos na memoria
+// caso queira salvar em um diretorio específico o comando "join(process.cwd(), 'src/schema.gql')" deve ser passado
+
+// sortSchema=> se true indexa os schemas em ordem alfabetica
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+  ],
 })
 export class AppModule {}
