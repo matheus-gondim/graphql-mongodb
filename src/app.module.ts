6eb3b4be063a12373a43128116a4bc0e87d48075
matheus-gondim <matheus-gondim>
2021-03-10 13:28:03 -0300

    add connection with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './entities/lesson.entity';
+import { LessonModule } from './lesson/lesson.module';
 
 // autoSchemaFile => define onde os schemas seram salvos, se true, eles seram salvos na memoria
-// caso queira salvar em um diretorio específico o comando "join(process.cwd(), 'src/schema.gql')" deve ser passado
+// caso queira salvar em um diretorio específico o comando "join(process.cwd(), 'src/schema.gql')" deve ser passado e "import { join } from 'path';" deve ser importado
 
 // sortSchema=> se true indexa os schemas em ordem alfabetica
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url:
+        'mongodb://localhost:27018/?readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
+    LessonModule,
   ],
 })
 export class AppModule {}
---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,18 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { LessonService } from './lesson.service';
+import { LessonType } from './lesson.type';
+
+@Resolver(() => LessonType)
+export class LessonResolver {
+  constructor(private readonly lessonService: LessonService) {}
+
+  @Query(() => LessonType)
+  lesson() {
+    return {
+      id: 'asdjo12j31a',
+      name: 'Physics Class',
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+}
